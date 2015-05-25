62584dfca9d4762eed28baf05abf76a2964b22dc
bxshi <baoxu.shi@gmail.com>
2015-05-25 10:42:24 -0400

    automatic deployment

---
+++ js/analytics_baidu-analytics.js
@@ -0,0 +1,1 @@
+var _hmt=_hmt||[];!function(){var e=document.createElement("script");e.src="//hm.baidu.com/hm.js?"+CONFIG.analytics.baidu;var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)}();
